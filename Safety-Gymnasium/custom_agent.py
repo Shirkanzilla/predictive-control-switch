8abbb4b50fb08ca0e6cffa4604b41c410ef5030c
Shirkanzilla <eichenrode@gmail.com>
2025-06-27 14:59:13 +0200

    Created the figures for the scond dataset as well

--- Safety-Gymnasium/custom_agent.py
+++ Safety-Gymnasium/custom_agent.py
@@ -1,35 +1,78 @@
 from omnisafe.models.actor import GaussianLearningActor
 import safety_gymnasium
-from safety_gymnasium.tasks.safe_navigation.goal.goal_level1 import GoalLevel1 
 import torch
 import numpy as np
+from math import sqrt
 from load_model import load_guide
+from safety_gymnasium.assets.geoms import Hazards
+from safety_gymnasium.assets.geoms import Sigwalls
+from safety_gymnasium.bases.base_task import BaseTask
+from safety_gymnasium.utils.registration import register
 
 import pdb
 import os
 os.environ["DISPLAY"] = ":0"
 
-class LowFrictionPointGoal(GoalLevel1):
-    def __init__(self, config):
+env_id = "SafetyPointSigWallGoalLevel1-v0"
+config = {'agent_name': 'Point'}
+kwargs = {'config': config, 'task_id': env_id}
+register(id=env_id, entry_point='omnisafe.envs.sagui_builder:SaguiBuilder', kwargs=kwargs, max_episode_steps=1000)
+
+coefs = None
+
+def set_coef_dict(coef_dict: dict):
+    global coefs  # bad practice but ok
+    coefs = coef_dict
+
+def _modify_dyn(model, coef_dict: dict):
+    for name, mult in coef_dict.items():
+        atr: np.ndarray = getattr(model, name)
+        atr[:] *= mult
+
+def _set_default_dyn(model):
+    model.dof_damping[0] *= 1.5  # Axis X
+    model.dof_damping[1] *= 1.5  # Axis Z
+    # model.dof_damping[2] *= 1.0  # Steering
+
+    if coefs != None:
+        _modify_dyn(model, coefs)
+
+
+class SigWallGoalLevel1(BaseTask):
+    """An agent must navigate to a goal."""
+
+    def __init__(self, config) -> None:
         super().__init__(config=config)
-        self.robot_model_file = os.path.abspath('custom_point.xml')
+
+        self._add_geoms(Hazards(num=1, keepout=0.75, size=0.7, locations=[(0, 0)]))
+        self._add_geoms(Sigwalls(num=4, locate_factor=2.5, is_constrained=True))
+
+        self.placements_conf.extents = [-1.75, -1.75, 1.75, 1.75]
+
+    def calculate_reward(self):
+        x0, y0, _ = self.last_robot_pos
+        x, y, _ = self.agent.pos
+        reward = sqrt((x - x0)**2 + (y - y0)**2)
+
+        return reward
+
+    def specific_reset(self):
+        self.last_robot_pos = self.agent.pos
+        _set_default_dyn(self.model)
+
+    def specific_step(self):
+        self.last_robot_pos = self.agent.pos
+
+    def update_world(self):
+        self.last_robot_pos = self.agent.pos
+
+    @property
+    def goal_achieved(self):
+        """Whether the goal of task is achieved."""
+        # pylint: disable-next=no-member
+        return self.dist_goal() <= self.goal.size
 
 
-
-env_id = 'LowFrictionPointGoal1-v0'
-
-config = {
-    'agent_name': 'Point',
-    'robot_model_file': 'custom_point.xml'
-}
-
-'''
-safety_gymnasium.register(
-    id=env_id,
-    entry_point='custom_agent:LowFrictionPointGoal',
-    kwargs={'config': config}
-)
-'''
 
 env = safety_gymnasium.make("SafetyPointGoal1-v0", config=config, render_mode="human")
 obs_space = env.observation_space
