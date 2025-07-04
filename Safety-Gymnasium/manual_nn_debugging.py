726ccc87feba844b4a7d8b8566fe5e225f4fd070
Shirkanzilla <eichenrode@gmail.com>
2025-07-04 17:34:03 +0200

    adapted manual debugging to the split and saved a trained classifier and regression model

--- Safety-Gymnasium/manual_nn_debugging.py
+++ Safety-Gymnasium/manual_nn_debugging.py
@@ -9,17 +9,39 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class ExpectedCostNN(nn.Module):
+class ExpectedCostRegressor(nn.Module):
     def __init__(self):
-        super(ExpectedCostNN, self).__init__()
+        super(ExpectedCostRegressor, self).__init__()
         self.fc = nn.Sequential(
-            nn.Linear(57, 64),
+            nn.Linear(57, 256),
             nn.ReLU(),
-            nn.Linear(64,1)
+            nn.Linear(256, 256),
+            nn.ReLU(),
+            nn.Linear(256,1),
         )
     def forward(self, x):
         return self.fc(x)
 
+class ExpectedCostClassifier(nn.Module):
+    def __init__(self):
+        super(ExpectedCostClassifier, self).__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(57, 128),
+            nn.ReLU(),
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Linear(128,1),
+            nn.Sigmoid()
+        )
+    def forward(self, x):
+        return self.fc(x)
+    
 class InputAction:
     def __init__(self):
         self.keys = {'i': False, 'j': False, 'k': False, 'l': False}
@@ -50,7 +72,7 @@ class InputAction:
             act[1] -= 1
         return act
 
-def predict_expected_cost(obs, action, model):
+def predict_expected_cost(obs, action, classifier, regressor):
     data = np.append(obs, action)
     # remove features not used in the model namely "velocimeter2", "accelerometer2", "magnetometer2", "gyro0" and "gyro1"
     data = np.delete(data, [2, 5, 6, 7, 11])
@@ -58,12 +80,19 @@ def predict_expected_cost(obs, action, model):
     data = data.unsqueeze(0)
     data = data.to(device)
     with torch.no_grad():
-        return model(data).item()
+        # return zero if the sample is classified as 0 class and the prediction of the regressor otherwise
+        if classifier(data).item() > 0.5:
+            return regressor(data).item()
+        else:
+            return 0
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = torch.load("/home/user/bachelor/Safety-Gymnasium/simple_model.pt")
-model.to(device)
-model.eval()
+classifier = torch.load("/home/user/bachelor/Safety-Gymnasium/classifier.pt")
+classifier.to(device)
+classifier.eval()
+regressor = torch.load("/home/user/bachelor/Safety-Gymnasium/regressor.pt")
+regressor.to(device)
+regressor.eval()
 
 env = safety_gymnasium.make('SafetyPointGoal1-v0', render_mode='human')
 obs, info = env.reset()
@@ -80,7 +109,7 @@ while True:
         obs, info = env.reset()
 
     act = input_act.get_act_from_input()
-    print(f"Expected cost: {predict_expected_cost(obs, act, model)}")
+    print(f"Expected cost: {predict_expected_cost(obs, act, classifier, regressor)}")
     obs, reward, cost, terminated, truncated, info = env.step(act)
 
     ep_ret += reward
